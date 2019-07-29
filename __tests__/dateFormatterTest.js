207a692e9fc1d559d017134173db6ff6da6d4235
seabasshoang <sylviahoang@hotmail.co.uk>
2019-07-29 17:43:34 +0100

    create dateFormatter function and complete format function

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,8 +1,13 @@
-import {format} from '../src/dateFormatter';
+import { format, dateFormatter } from "../src/dateFormatter";
 
 it('when the system date and the date to format are the same day formats as "TODAY"', () => {
   const December = 11; //js Date object month is indexed from 0
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
-  expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("TODAY");
+});
+
+it("input date follows the initail format DD/MM/YYYY", () => {
+  const date = "2018-12-15";
+  expect(dateFormatter(date)).toBe("15/12/2018");
 });
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,13 @@
+export const dateFormatter = date => {
+  let formattedDate = date
+    .split("-")
+    .reverse()
+    .join("/");
+  return formattedDate;
+};
+
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  let todaysDate = new Date(systemDateTimeMillis).toLocaleDateString();
+  let inputDate = new Date(dateToFormatTimeMillis).toLocaleDateString();
+  return todaysDate === inputDate ? "TODAY" : dateFormatter(inputDate);
 };
